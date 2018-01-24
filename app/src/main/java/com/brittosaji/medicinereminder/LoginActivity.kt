018813b126ad9dfd444683fa8cd2548dad04349d
brittosaji19 <bs1067@gmail.com>
2018-01-24 16:52:33 +0530

    Added Login

---
+++ app/src/main/java/com/brittosaji/medicinereminder/LoginActivity.kt
@@ -0,0 +1,89 @@
+package com.brittosaji.medicinereminder
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private val RC_SIGNIN:Int=123
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val googleSignInOptions: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.client_id)).requestEmail().build()
+        val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+        mAuth= FirebaseAuth.getInstance()
+        val signInWithGoogleButton = googleSignInButton
+        signInWithGoogleButton.setOnClickListener(View.OnClickListener {
+            signInWithGoogle(googleSignInClient)
+        })
+        val signInButton=signInButton
+        signInButton.setOnClickListener {
+            signInWithEmail(emailEditText.text.toString(),passwordEditText.text.toString())
+        }
+    }
+
+    private fun signInWithGoogle(signInClient: GoogleSignInClient){
+        Toast.makeText(this,"Starting Google SignIn", Toast.LENGTH_SHORT).show()
+        val signInIntent=signInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGNIN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==RC_SIGNIN){
+            val task: Task<GoogleSignInAccount>
+            task=GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account: GoogleSignInAccount =task.getResult(ApiException::class.java)
+                firebaseSignInWithGoogle(account)
+                Toast.makeText(this,"Signed In Succesfully",Toast.LENGTH_SHORT).show()
+            }
+            catch (e:Exception){
+                Toast.makeText(this,"SignIn Failed",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseSignInWithGoogle(account:GoogleSignInAccount){
+        Toast.makeText(this,"Logging In As "+ account.id,Toast.LENGTH_SHORT).show()
+        val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken,null)
+        val authHandler=mAuth.signInWithCredential(credential)
+        authHandler.addOnCompleteListener(this, OnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(this,"Logged In",Toast.LENGTH_SHORT).show()
+                val user=mAuth.currentUser
+            }
+            else{
+                Toast.makeText(this,"Authentication Failed",Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun signInWithEmail(email:String,pass:String){
+        val signInHandler=mAuth.signInWithEmailAndPassword(email,pass)
+        signInHandler.addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(this,"Logged in as : " +mAuth.currentUser,Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Authentication Failure",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/brittosaji/medicinereminder/SplashActivity.kt
+++ app/src/main/java/com/brittosaji/medicinereminder/SplashActivity.kt
@@ -7,9 +7,10 @@ import android.os.Handler
 import android.support.v7.app.ActionBar
 import android.view.Window
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
-
+    val mAuth=FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -19,8 +20,14 @@ class SplashActivity : AppCompatActivity() {
         handler.postDelayed(Runnable {
 //            var toast=Toast.makeText(this,"Test Toast",Toast.LENGTH_LONG)
 //            toast.show()
-            var intentForMainActivity=Intent(this,MainActivity::class.java)
-            startActivity(intentForMainActivity)
+            if (mAuth.currentUser==null){
+                var loginIntent=Intent(this,LoginActivity::class.java)
+                startActivity(loginIntent)
+            }
+            else {
+                var intentForMainActivity = Intent(this, MainActivity::class.java)
+                startActivity(intentForMainActivity)
+            }
 
         },2000)
 
